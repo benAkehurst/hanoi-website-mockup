9e1573b5e20178d669234f782ea20e2ce5f332e4
mistabenjo <benakehurst@hotmail.com>
2017-03-30 20:21:59 +0300

    new footer

--- app/aboutModule.js
+++ app/aboutModule.js
@@ -12,12 +12,14 @@
 		// https://maps.googleapis.com/maps/api/place/details/json?placeid=ChIJ096sZJxMHRUROKs1iZd1Xps&key=AIzaSyBKs2JPf2BmSP9RkZyBlvGtwWU1UjV5IGI
 
 		$http({
-		  method: 'GET',
-		  url: 'https://maps.googleapis.com/maps/api/place/details/json?placeid=ChIJ096sZJxMHRUROKs1iZd1Xps&key=AIzaSyBKs2JPf2BmSP9RkZyBlvGtwWU1UjV5IGI'
-		}).then(function successCallback(response) {
-		    var reviews = response.data.result.reviews;
-		    console.log(reviews);
-		  });
+			method: 'GET',
+			url: 'https://maps.googleapis.com/maps/api/place/details/json?placeid=ChIJ096sZJxMHRUROKs1iZd1Xps&key=AIzaSyBKs2JPf2BmSP9RkZyBlvGtwWU1UjV5IGI'})
+			
+			.then(function successCallback(response) {
+		    	var reviews = response.data.result.reviews;
+		    	console.log(reviews);
+		  	});
+		  	
 
 	});
 
