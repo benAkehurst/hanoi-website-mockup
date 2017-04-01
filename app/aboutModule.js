28e1ed056797517b0fafc714407996c98d70ea85
mistabenjo <benakehurst@hotmail.com>
2017-04-01 13:31:24 +0300

    about page working

--- app/aboutModule.js
+++ app/aboutModule.js
@@ -5,7 +5,7 @@
 	var aboutModule = angular.module("aboutModule", []);
 
 	aboutModule.controller("AboutController", function($scope, $http){
-		
+
 		// https://developers.google.com/places/web-service/details
 		// AIzaSyBKs2JPf2BmSP9RkZyBlvGtwWU1UjV5IGI - Google Details API Key 
 		// https://maps.googleapis.com/maps/api/place/details/json?placeid=ChIJ096sZJxMHRUROKs1iZd1Xps&key=AIzaSyBKs2JPf2BmSP9RkZyBlvGtwWU1UjV5IGI
@@ -16,10 +16,10 @@
 			
 			.then(function successCallback(response) {
 		    	// Testing Response to find correct element
-		    	// var reviews = response.data.result.reviews;
+		    	var reviews = response.data.result;
 		    	// var reviewerName = response.data.result.reviews[0].author_name;
 		    	// var reviewerText = response.data.result.reviews[0].text;
-		    	// console.log(reviews);
+		    	console.log(reviews);
 		    	// console.log(reviewerName);
 		    	// console.log(reviewerText);
 
